93e7c3a6fa033cf761afefd1a34e3321ffb747ba
unknown <prantokih42@gmail.com>
2024-12-29 22:53:25 +0600

    basic route created

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='bg-red-600 text-center text-7xl'>
+            this is home page
+        </div>
+    );
+};
+
+export default Home;
