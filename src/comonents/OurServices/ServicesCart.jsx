46d8d0ade6a1e63df881121da9b701536e5f208f
unknown <prantokih42@gmail.com>
2025-01-11 22:50:46 +0600

    button eye icon added

--- src/comonents/OurServices/ServicesCart.jsx
+++ src/comonents/OurServices/ServicesCart.jsx
@@ -1,20 +1,29 @@
 import React from "react";
+import { FaRegEye } from "react-icons/fa";
 
 const ServicesCart = ({ services }) => {
   const { name, images, description } = services;
+
   return (
-    <div className="card shadow-md hover:shadow-lg transition-shadow duration-300 shadow-purple-200">
-      <div className="md:flex  flex-grow items-center justify-between p-4 bg-white rounded-md">
+    <div className="relative group card shadow-md rounded-md overflow-hidden">
+      {/* Main Content */}
+      <div className="md:flex flex-grow items-center justify-between p-4 bg-white">
         <div className="p-2 gap-5">
           <div className="image-container">
             <img className="services-image" src={images} alt={name} />
           </div>
           <div className="mt-5">
-            <h3 className="uppercase font-bold ">{name}</h3>
-            <p className=" text-gray-600">{description}</p>
+            <h3 className="uppercase font-bold">{name}</h3>
+            <p className="text-gray-600">{description}</p>
           </div>
         </div>
-     
+      </div>
+
+      {/* Shadow Overlay with Eye Icon */}
+      <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 -top-full group-hover:top-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
+        <button>
+          <FaRegEye className="absolute bottom-2 right-4 text-purple-100 border-2 border-purple-100 text-4xl" />
+        </button>
       </div>
     </div>
   );
